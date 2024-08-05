b6b4d034d504ea5c29b5b838483ae62089e1fad6
aerochops <150325960+aerochops@users.noreply.github.com>
2024-08-05 19:31:41 +0700

    fixing API PageDetail

--- src/views/detail/page-detail.vue
+++ src/views/detail/page-detail.vue
@@ -1,8 +1,9 @@
 <template>
   <div>ini testing</div>
+  <pre>{{ movie.name }}</pre>
 </template>
 
-<script>
+<script setup>
 import { useApi } from "@/functions/api";
 import { ref } from "vue";
 import { useRoute } from "vue-router";
@@ -14,7 +15,20 @@ const route = useRoute();
 const getData = async () => {
   const response = await api.GET(`shows/${route.params.id}`);
   console.log(response);
+  movie.value = {
+    id: response.id,
+    name: response.name,
+    image: response.image.original,
+    rating: response.rating.average,
+    summary: response.summary,
+    type: response.type,
+    status: response.status,
+    language: response.language,
+    genres: response.genres,
+  };
 };
+
+getData();
 </script>
 
 <style></style>
