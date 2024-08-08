e03854f4467146aa8f79d8ba1b473180a42a94b9
aerochops <150325960+aerochops@users.noreply.github.com>
2024-08-08 16:17:31 +0700

    menambaahkan api casts

--- src/views/detail/page-detail.vue
+++ src/views/detail/page-detail.vue
@@ -17,8 +17,15 @@
         <div>{{ movie.rating }}/10</div>
       </div>
       <div>
-        {{ movie.summary }}
+        <div>{{ stripHTML(movie.summary) }}</div>
       </div>
+      <div>
+        Starring:
+        <span>
+          {{ starring }}
+        </span>
+      </div>
+      <div>Directed By:{{}}</div>
     </div>
   </div>
 </template>
@@ -26,17 +33,21 @@
 <script setup>
 import AppImage from "@/components/app-image.vue";
 import { useApi } from "@/functions/api";
-import { ref } from "vue";
+import { computed, ref } from "vue";
 import { useRoute } from "vue-router";
 import imdbLogo from "@/assets/imdb-logo.svg";
+import { stripHTML } from "@/functions/html-parser";
 
 const movie = ref([]);
 const api = useApi();
 const route = useRoute();
+const casts = ref([]);
 
 const getData = async () => {
   const response = await api.GET(`shows/${route.params.id}`);
+  const responseCast = await api.GET(`shows/${route.params.id}/cast`);
   console.log(response);
+  console.log(responseCast);
   movie.value = {
     id: response.id,
     name: response.name,
@@ -49,7 +60,21 @@ const getData = async () => {
     genres: response.genres,
     premiered: response.premiered.split("-")[0],
   };
+
+  casts.value = responseCast.map((cast) => {
+    return {
+      id: cast.person.id,
+      name: cast.person.name,
+    };
+  });
 };
+
+const starring = computed(() => {
+  return casts.value
+    .filter((cast) => cast.id === 1 || cast.id === 2)
+    .map((cast) => cast.name)
+    .join(", ");
+});
 
 getData();
 </script>
