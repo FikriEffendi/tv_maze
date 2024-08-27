af5c56ef89e20f340de032a5f0ff76f848329946
aerochops <150325960+aerochops@users.noreply.github.com>
2024-08-28 00:15:50 +0700

    membuat carousel di section-popular

--- src/views/home/section-popular.vue
+++ src/views/home/section-popular.vue
@@ -1,31 +1,41 @@
 <template>
   <div>
     <div>Popular Show</div>
-    <div class="flex gap-5 overflow-auto">
-      <template v-for="movie in PopularShow" :key="movie.id">
+    <Swiper
+      :slides-per-view="4.5"
+      :navigation="true"
+      :modules="modules"
+      class="m-4"
+    >
+      <SwiperSlide v-for="movie in PopularShow.slice(0, 10)" :key="movie.id">
         <div class="flex gap-2">
           <div class="self-center text-3xl font-bold">1</div>
           <div class="bg-black rounded-lg size-24"></div>
           <div>
             <div class="text-xs">PG-13</div>
-            <div>Nama</div>
+            <div>{{ movie.name }}</div>
             <div class="flex items-baseline gap-1">
               <div>Horor</div>
               <div class="bg-black rounded-full size-1"></div>
               <div>Fantasy</div>
             </div>
-            <div>Rating</div>
+            <div>{{ movie.rating }}</div>
           </div>
         </div>
-      </template>
-    </div>
+      </SwiperSlide>
+    </Swiper>
   </div>
 </template>
 
 <script setup>
 import { useApi } from "@/functions/api";
 import { ref } from "vue";
-import SwiperSlider from "@/components/swiper-slider.vue";
+import { Swiper, SwiperSlide } from "swiper/vue";
+import "swiper/css";
+import "swiper/css/navigation";
+import { Navigation } from "swiper/modules";
+
+const modules = [Navigation];
 
 const PopularShow = ref([]);
 
@@ -35,7 +45,7 @@ const getData = async () => {
   const response = await api.GET("shows");
 
   PopularShow.value = response
-    .filter((movie) => movie.rating.average >= 8.0)
+    .filter((movie) => movie.rating.average >= 8.5)
     .map((movie) => {
       return {
         id: movie.id,
