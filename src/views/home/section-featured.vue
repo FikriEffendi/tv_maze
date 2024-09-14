c8512326915996632682b75b05d785eea1d5ff77
aerochops <150325960+aerochops@users.noreply.github.com>
2024-09-14 23:57:17 +0700

    testing backgroud di section-featured

--- src/views/home/section-featured.vue
+++ src/views/home/section-featured.vue
@@ -1,9 +1,10 @@
 <template>
   <div>
     <template>
-      <div>
-        <div></div>
-        <div></div>
+      <div class="bg-slate-300">
+        Halo
+        <div class="bg-red-200"></div>
+        <div class="bg-blue-200"></div>
       </div>
     </template>
   </div>
@@ -11,12 +12,24 @@
 
 <script setup>
 import { useApi } from "@/functions/api";
+import { ref } from "vue";
 
 const api = useApi();
+const movies = ref([]);
 
 const getData = async () => {
   const response = await api.GET("shows");
+
+  movies.value = response.map((movie) => {
+    return {
+      id: movie.id,
+      name: movie.name,
+      image: movie.image.original,
+    };
+  });
 };
+
+getData();
 </script>
 
 <style lang="scss" scoped></style>
