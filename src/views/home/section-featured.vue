de77dc5654c66bd45a5455d7379e8f163619ec22
aerochops <150325960+aerochops@users.noreply.github.com>
2024-09-18 14:53:08 +0700

    memperbaiki bug di section featured I

--- src/views/home/section-featured.vue
+++ src/views/home/section-featured.vue
@@ -1,10 +1,12 @@
 <template>
-  <div>
-    <div class="bg-yellow-400 h-[40rem] items-center">
+  <div class="relative">
+    <div class="absolute inset-x-0 bg-yellow-400 h-[42rem] flex items-center">
       <div class="w-full h-[39rem] bg-slate-300">
         Halo
+
         <div class="flex h-[37rem] justify-center">
           <div class="w-1/2 bg-red-200"></div>
+
           <div class="w-1/2 bg-blue-200"></div>
         </div>
       </div>
@@ -14,24 +16,30 @@
 
 <script setup>
 import { useApi } from "@/functions/api";
+
 import { ref } from "vue";
 
 const api = useApi();
+
 const movies = ref([]);
 
 const getData = async () => {
   const response = await api.GET("shows");
 
-  movies.value = response.map((movie) => {
-    return {
-      id: movie.id,
-      name: movie.name,
-      image: movie.image.original,
-    };
-  });
+  movies.value = response.map((movie) => ({
+    id: movie.id,
+
+    name: movie.name,
+
+    image: movie.image.original,
+  }));
 };
 
 getData();
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+/* Tambahkan gaya tambahan jika diperlukan */
+</style>
+
+Saya sudah mengikuti kodingan dari anda kenapa
