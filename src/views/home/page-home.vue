ecf7215811b943f673962b3f66829c0394cea246
aerochops <150325960+aerochops@users.noreply.github.com>
2024-09-23 01:44:05 +0700

    menambahkan shadow di section featured

--- src/views/home/page-home.vue
+++ src/views/home/page-home.vue
@@ -4,9 +4,9 @@
     <template v-else>
       <div class="space-y-10">
         <section-running class="container" />
-        <section-popular class="container" />
-        <section-featured />
-        <section-random />
+        <section-popular class="container shadow" />
+        <section-featured class="" />
+        <section-random class="container shadow" />
       </div>
     </template>
   </div>
