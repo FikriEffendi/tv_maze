fb72f45e49347e45a87d018377a81ac011734544
aerochops <150325960+aerochops@users.noreply.github.com>
2024-12-27 22:46:48 +0700

    membuat logic section-genre dan menginisiasi page-genre-detail

--- src/views/genre/page-genre.vue
+++ src/views/genre/page-genre.vue
@@ -1,9 +1,13 @@
 <template>
-  <section-genre class="container"></section-genre>
+  <div class="space-y-3">
+    <genre-page-selector class="container"></genre-page-selector>
+    <section-genre class=""></section-genre>
+  </div>
 </template>
 
 <script setup>
 import SectionGenre from "./section-genre.vue";
+import GenrePageSelector from "@/components/genre-page-selector.vue";
 </script>
 
 <style lang="scss" scoped></style>
--- src/views/genre/section-genre.vue
+++ src/views/genre/section-genre.vue
@@ -1,19 +1,41 @@
 <template>
-  <div class="flex justify-center">
-    <div
-      class="grid grid-cols-3 text-sm font-bold border-2 border-black divide-x-2 divide-black justify-items-center rounded-3xl w-70"
-    >
-      <div class="column-config">halo Test</div>
-      <div class="column-config">halo Test</div>
-      <div class="column-config">halo Test</div>
+  <div>
+    <div class="divide-y-2 divide-slate-400">
+      <div
+        v-for="movieGenre in genreList"
+        :key="movieGenre.id"
+        class="py-5 text-xl font-bold hover:bg-green-700 hover:text-white"
+      >
+        <div class="container">{{ movieGenre.genre }}</div>
+      </div>
     </div>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { useApi } from "@/functions/api";
+import { ref } from "vue";
 
-<style lang="postcss" scoped>
-.column-config {
-  @apply px-2 py-2 h-full;
-}
-</style>
+const api = useApi();
+
+const genreList = ref([]);
+
+const getData = async () => {
+  const response = await api.GET("shows");
+
+  response.forEach((movie) => {
+    movie.genres.forEach((genre) => {
+      if (!genreList.value.some((item) => item.genre === genre)) {
+        genreList.value.push({
+          id: movie.id,
+          genre: genre,
+        });
+      }
+    });
+  });
+};
+
+getData();
+</script>
+
+<style lang="postcss" scoped></style>
